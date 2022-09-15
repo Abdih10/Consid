35c0a528ff719b9766ec1638c0e440edccbc71cd
Abdi <abdi_665@hotmail.com>
2022-09-15 18:08:38 +0200

    initial commit

---
+++ src/main/java/com/example/consid/ConsidApplication.java
@@ -0,0 +1,13 @@
+package com.example.consid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsidApplication.class, args);
+    }
+
+}
